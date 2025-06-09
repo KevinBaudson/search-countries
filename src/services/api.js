007bd5b9d70dea69d4cff64db39dd273317335a9
KevinBaudson <kevinbaudson@hotmail.com>
2025-06-09 19:16:21 -0300

    ajustando api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,22 @@
 const BASE_URL = "https://restcountries.com/v3.1";
 
+const FIELDS = "name,cca3,flags,population,region,capital";
+
 export async function getAllCountries() {
-  const response = await fetch(`${BASE_URL}/all`);
+  const response = await fetch(`${BASE_URL}/all?fields=${FIELDS}`);
   if (!response.ok) throw new Error("Failed to fetch countries");
   return response.json();
 }
 
 export async function getCountryByName(name) {
-  const response = await fetch(`${BASE_URL}/name/${name}`);
+
+  const response = await fetch(`${BASE_URL}/name/${name}?fields=${FIELDS}`);
   if (!response.ok) throw new Error("Country not found");
   return response.json();
 }
 
 export async function getCountriesByRegion(region) {
-  const response = await fetch(`${BASE_URL}/region/${region}`);
+  const response = await fetch(`${BASE_URL}/region/${region}?fields=${FIELDS}`);
   if (!response.ok) throw new Error("Failed to fetch countries by region");
   return response.json();
 }
